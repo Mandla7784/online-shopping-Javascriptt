8ac12ee629a95669f6b53d446aec8f88913c3591
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2024-07-23 14:42:24 +0200

    Rendered items on screen

--- online-shopping/main.js
+++ online-shopping/main.js
@@ -5,12 +5,30 @@ import URLS from "./util/urlCongiguration";
 //delclaring and assigning vlues to dom elements
 const productContainer = document.getElementById("product-list");
 const { endpoints } = URLS; // excracting the endpoints using object desctructuring
-console.log(endpoints);
+
 //function to fetchItems from API / sever side
 async function fetchAllItems() {
   const data = await fetchData(`${URLS.baseURL}${endpoints.getAllProducts}`); //fetching all items from server
-  console.log(data);
+  //invoking the display function
+  displayAllItems(data);
+}
+/// function  to render items in the DOM
+async function displayAllItems(data) {
+  //maping through the data list and render as a card
+  const itemList = data.map((item) => {
+    const { image, title } = item;
+    return /*html*/ `
+          <div class="card ">
+            <img class="w-full h-48 object-cover"  src=${image}  alt=${title} />
+            <ionic-icon name="cart-outline"></ionic-icon>
+            <iconic-icon name="heart-outline"></ionic-icon>
+            <h1>${title}</h1>
+            <a class="text-slate-700 hover:text-slate-900 transition">Read More</a>
+          </div>
+        
+        `;
+  });
+  productContainer.innerHTML = itemList.join("");
 }
 
-/// function  to render items in the DOM
-async function displayAllItems(data) {}
+fetchAllItems();
