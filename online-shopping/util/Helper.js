cd42cceb08b458b669fe9f45443ea55ba49a93d5
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2024-07-23 14:13:20 +0200

    Updates

--- online-shopping/util/Helper.js
+++ online-shopping/util/Helper.js
@@ -2,9 +2,11 @@
 async function fetchData(url) {
   try {
     const response = await fetch(url); // fetches the data from Api
+
     if (response.ok) {
       // check data if it exists and return as JSON object
-      const data = await response.json();
+
+      const data = response.json();
       return data;
     }
   } catch (error) {
