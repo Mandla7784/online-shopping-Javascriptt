edfd1e2fac214e40647a973db1cba46a4ade6644
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2024-07-23 14:00:16 +0200

    Updated the script file main.js

--- online-shopping/main.js
+++ online-shopping/main.js
@@ -4,9 +4,12 @@ import URLS from "./util/urlCongiguration";
 
 //delclaring and assigning vlues to dom elements
 const productContainer = document.getElementById("product-list");
-
+const { endpoints } = URLS; // excracting the endpoints using object desctructuring
+console.log(endpoints);
 //function to fetchItems from API / sever side
-
 async function fetchAllItems() {
-  const data = await fetchData(URLS.baseURL);
+  const data = await fetchData(`${URLS} ${endpoints.getAllProducts}`); //fetching all items from server
+  console.log(data);
 }
+
+fetchAllItems();
