120e33e89e47ef75c81c78e5ab0624a131e918ae
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2024-07-23 11:24:52 +0200

    checked if data exists and parse it to json object

--- online-shopping/util/Helper.js
+++ online-shopping/util/Helper.js
@@ -1,5 +1,14 @@
 //function to fetch data from Server to client using RESTful API
 
 async function fetchData(url) {
-  const response = await fetch(url);
+  try {
+    const response = await fetch(url); // fetches the data from Api
+    if (response.ok) {
+      // check data if it exists and return as JSON object
+      const data = response.json();
+      return data;
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }
